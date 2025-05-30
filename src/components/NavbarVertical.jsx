494930c12d2361c2f0c07d06a17978982447094c
Marius_DIATTA <marius@kayfo.games>
2025-05-30 02:47:43 +0200

    Dropdown down small screen

--- src/components/NavbarVertical.jsx
+++ src/components/NavbarVertical.jsx
@@ -11,6 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setCurrentNav } from '../slices/navigateSlice'
 import ProfileCard from "./ProfilUser";
 import api from "../services/Axios";
+import { useEffect, useRef, useState } from "react";
 
 
 const lesAccount = async () => {
@@ -110,11 +111,15 @@ const menuItems = [
 
 const VertcalNavbar = ({ children }) => {
 
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+
     const dispatch = useDispatch();
 
     const current = useSelector(state => state.navigate.currentNav);
 
     const previous = useSelector(state => state.navigate.previousNav);
+
+    const sidebarRef = useRef();
 
     const updateActiveTab = (tab) => {
 
@@ -122,6 +127,22 @@ const VertcalNavbar = ({ children }) => {
 
         lesAccount()
     }
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+                setSidebarOpen(false);
+            }
+        }
+
+        if (isSidebarOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isSidebarOpen]);
 
     const tabContent = {
 
@@ -150,17 +171,37 @@ const VertcalNavbar = ({ children }) => {
     return (
         < >
 
-            <button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-
-                <span className="sr-only">Open sidebar</span>
-
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            {/* Toggle Button */}
+            <button
+                onClick={() => setSidebarOpen(!isSidebarOpen)}
+                type="button"
+                className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
+                <span className="sr-only">Toggle sidebar</span>
+                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                     <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
-
             </button>
 
-            <aside id="separator-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen  transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            {/* Sidebar */}
+            <aside
+                id="separator-sidebar"
+                ref={sidebarRef}
+                className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${isSidebarOpen ? '' : '-translate-x-full'} sm:translate-x-0`}
+                aria-label="Sidebar"
+            >
+
+            {/*<button data-drawer-target="separator-sidebar" data-drawer-toggle="separator-sidebar" aria-controls="separator-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">*/}
+
+            {/*    <span className="sr-only">Open sidebar</span>*/}
+
+            {/*    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">*/}
+            {/*        <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>*/}
+            {/*    </svg>*/}
+
+            {/*</button>*/}
+
+            {/*<aside id="separator-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen  transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">*/}
 
                 <div className="scrollbor_hidden h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 ">
 
@@ -300,7 +341,7 @@ const VertcalNavbar = ({ children }) => {
                         id={`${current}-tab`}
                         role="tabpanel"
                         aria-labelledby={`${current}-tab-button`}
-                        className="mb-5 bg-white dark:bg-gray-800 rounded-lg shadow-md max-w-full h-full pb-2"
+                        className="mb-5 bg-white dark:bg-gray-800 rounded-lg shadow-md max-w-full h-full m-1 pb-1"
                     >
                         {tabContent[current]}  
 
