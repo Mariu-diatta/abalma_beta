e66be8a2a08f04d067f6f378683f1605d704f236
Marius_DIATTA <marius@kayfo.games>
2025-06-18 00:15:12 +0200

    filters users contact

--- src/components/ContactUser.jsx
+++ src/components/ContactUser.jsx
@@ -11,11 +11,12 @@ const UserTable = () => {
     const [selectedUsers, setSelectedUsers] = useState([]);
     const [users, setUsers] = useState([]);
     const userSelected = useSelector(state => state.chat.userSlected)
+    const currentuser = useSelector(state => state.auth.user)
 
     useEffect(
         () => {
 
-            const apiUsers =async () => {
+            const apiUsers = async () => {
 
                 try {
 
@@ -40,7 +41,7 @@ const UserTable = () => {
 
             apiUsers()
         }
-        
+
         , []
     )
 
@@ -63,9 +64,9 @@ const UserTable = () => {
 
             user.email.toLowerCase().includes(searchTerm);
 
-            const matchesStatus = statusFilter === "Tous" || user?.is_connected;
+        const matchesStatus = statusFilter === "Tous" || user?.is_connected;
 
-            return matchesSearch && matchesStatus;
+        return matchesSearch && matchesStatus;
     });
 
     const isAllSelected = filteredUsers.length > 0 && filteredUsers.every(user => selectedUsers.includes(user.email));
@@ -213,7 +214,7 @@ const UserTable = () => {
 
                     {filteredUsers.map((user, i) => (
 
-                        !(user?.id === userSelected?.id) &&
+                        !(user?.id === currentuser?.id) &&
 
                         <tr key={i} className="bg-white  dark:bg-gray-800  hover:bg-gray-50 dark:hover:bg-gray-600">
 
@@ -245,13 +246,13 @@ const UserTable = () => {
 
                             </td>
 
-                                <td className="px-6 py-4">{user?.description}</td>
+                            <td className="px-6 py-4">{user?.description}</td>
 
                             <td className="px-6 py-4">
 
                                 <svg className="w-6 h-5 text-red-800 dark:text-white cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                    
-                                     <path fillRule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clipRule="evenodd" />
+
+                                    <path fillRule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clipRule="evenodd" />
 
                                 </svg>
 
