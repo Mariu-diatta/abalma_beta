557e132357e1e5441d4d1bfbdf5a1c058d6bec3d
Marius_DIATTA <marius@kayfo.games>
2025-06-18 00:09:47 +0200

    update component users contacts: add popover on icon btn

--- src/components/OwnerProfil.jsx
+++ src/components/OwnerProfil.jsx
@@ -235,6 +235,11 @@ const OwnerAvatar = ({ owner }) => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor"
                         viewBox="0 0 24 24"
+                        title={owner.nom}
+                        onClick={() => {
+
+                            setShowPopover((prev) => !prev);
+                        }}
 
                     >
                         <path
