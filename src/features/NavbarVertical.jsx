c1abc9e1951e4da95f05c1144d11cd5fc05d5a14
Marius_DIATTA <marius@kayfo.games>
2025-06-20 01:19:21 +0200

    update navigate vertical menu

--- src/features/NavbarVertical.jsx
+++ src/features/NavbarVertical.jsx
@@ -198,9 +198,10 @@ const VertcalNavbar = ({ children }) => {
 
                                 <span className="flex-1 ms-3 whitespace-nowrap">Accueil</span>
 
-                                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm  text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                                {currentUser?.is_pro && <span className="inline-flex items-center justify-center px-2 ms-3 text-sm  text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>}
 
                             </span>
+
                         </li>
 
                         <li>
@@ -249,23 +250,26 @@ const VertcalNavbar = ({ children }) => {
        
                     </ul>
 
-                    <ul className="pt-4 mt-4 space-y-2  border-t border-gray-200 dark:border-gray-700 cursor-pointer">
+                    {
+                        !currentUser?.is_pro &&
+                        <ul className="pt-4 mt-4 space-y-2  border-t border-gray-200 dark:border-gray-700 cursor-pointer">
 
-                        <li>
-                            <span className="flex items-center p-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <li>
+                                <span className="flex items-center p-2 text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
+                                    <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
 
-                                    <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
+                                        <path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z" />
 
-                                </svg>
+                                    </svg>
 
-                                <span className="ms-3" onClick={()=>alert("SERVICE PAS ENCORE DISPONIBLE") }>Upgrade to Pro</span>
+                                    <span className="ms-3" onClick={()=>alert("SERVICE PAS ENCORE DISPONIBLE") }>Upgrade to Pro</span>
 
-                            </span>
-                        </li>
+                                </span>
+                            </li>
 
-                    </ul>
+                        </ul>
+                    }
 
                     <ul className="scrollbor_hidden h-full lg:h-[300px] py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800  mt-1 space-y-1  border-t border-gray-200 dark:border-gray-700">
 
