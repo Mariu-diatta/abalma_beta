eff56eb10bda37932b87d0bee40fbbb23e18744c
Marius_DIATTA <marius@kayfo.games>
2025-07-07 14:16:20 +0200

    update PersistLogin.jsx

--- src/features/auth/PersistLogin.jsx
+++ src/features/auth/PersistLogin.jsx
@@ -4,60 +4,87 @@ import { useDispatch } from "react-redux";
 import { login } from "../../slices/authSlice";
 import api from "../../services/Axios";
 
-const getNewToken = async (refreshToken) => {
-    try {
-        const response = await api.post("refresh/", { refresh: refreshToken });
-        const access = response.data;
+const getNewToken = async (refreshToken_) => {
 
-        if (access) {
-            localStorage.setItem("token", access?.access);
-            return access;
+    try {
+
+        const response = await api.post("refresh/", { refresh: refreshToken_ });
+
+        const accessToken = response.data;
+
+        if (accessToken) {
+
+            localStorage.setItem("token", accessToken?.access);
+
+            return accessToken;
         }
 
         return null;
+
     } catch (error) {
+
         console.error("Erreur lors du rafraîchissement du token :", error);
+
         return null;
     }
 };
 
 const isRealReload = () => {
+
     const [entry] = performance.getEntriesByType("navigation");
+
     return entry?.type === "reload";
 };
 
 const PersistLogIn = () => {
+
     const [isLoading, setIsLoading] = useState(isRealReload());
+
     const dispatch = useDispatch();
 
     useEffect(() => {
+
         const verifyRefreshToken = async () => {
+
             try {
+
                 const refreshToken = localStorage.getItem("refresh");
+
                 if (!refreshToken) return;
 
                 const newTokens = await getNewToken(refreshToken);
+
                 if (!newTokens?.access) return;
 
                 const response = await api.get("about/me/");
+
                 dispatch(login(response.data));
 
             } catch (error) {
+
                 console.error("Échec du rafraîchissement ou de la récupération utilisateur :", error);
+
             } finally {
+
                 setIsLoading(false);
             }
         };
 
         if (isRealReload()) {
+
             verifyRefreshToken();
         }
+
     }, [dispatch]);
 
     if (isLoading) {
+
         return (
+
             <div style={{ textAlign: "center", marginTop: "2rem" }}>
+
                 <p>Chargement...</p>
+
             </div>
         );
     }
