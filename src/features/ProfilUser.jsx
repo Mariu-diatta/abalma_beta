f8d4867fcd5d90cca31efb6c3312dc5999691c3f
Marius_DIATTA <marius@kayfo.games>
2025-07-12 17:09:05 +0200

    clean code : profilUser

--- src/features/ProfilUser.jsx
+++ src/features/ProfilUser.jsx
@@ -329,7 +329,7 @@ const ProfileCard = () => {
                             aria-label={t('ProfilText.modifierCouverture')}
                     >
                             <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
                             </svg>
 
                     </button>
