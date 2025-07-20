34053e38f4c2102efe7a1b6c430d1a13d2aa3426
Marius_DIATTA <marius@kayfo.games>
2025-07-20 06:00:11 +0200

    change about path

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -266,7 +266,7 @@ const NavbarHeader = () => {
         {
             id: 'about',
             label: t('about'),
-            endPoint: '/About',
+            endPoint: '/about',
             logo: <svg className="w-[22px] h-[22px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
