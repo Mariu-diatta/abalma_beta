766039e5efc6d7dbcd53478127deb2b672229567
Marius_DIATTA <marius@kayfo.games>
2025-07-21 09:50:50 +0200

    add icon trash in ContactUser

--- src/components/ContactUser.jsx
+++ src/components/ContactUser.jsx
@@ -202,7 +202,7 @@ const UsersContactsList = () => {
             </div>
 
             {/* Tableau */}
-            <table className="w-auto text-sm text-left style-bg shadow-lg ">
+            <table className="w-full text-sm text-left style-bg shadow-lg ">
 
                 <thead className="text-xs uppercase style-bg">
 
@@ -229,7 +229,7 @@ const UsersContactsList = () => {
 
                 </thead>
 
-                <tbody className="overflow-x-auto">
+                <tbody>
 
                     {filteredUsers.map((user, i) => (
 
@@ -270,10 +270,8 @@ const UsersContactsList = () => {
 
                             <td className="px-6 py-4">
 
-                                <svg className="w-6 h-5 text-red-1000 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-
-                                    <path fillRule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clipRule="evenodd" />
-
+                                <svg className="w-6 h-6 text-gray-800 dark:text-red-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                 </svg>
 
                             </td>
