abbadc6858403e235214308a5f0ed9fda071467b
Marius_DIATTA <marius@kayfo.games>
2025-07-23 07:22:07 +0200

    liste blogs in Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -53,7 +53,7 @@ const BlogPage = () => {
 
                     <div className="grid gap-8 lg:grid-cols-2">
 
-                        {blogPosts.map((post, index) => (
+                        {blogs.map((post, index) => (
                             <BlogCard key={index} {...post} />
                         ))}
 
@@ -71,39 +71,29 @@ export default BlogPage;
 
 
 //la carte article 
-const BlogCard = ({
-    category,
-    timeAgo,
-    title,
-    description,
-    author,
-    avatarUrl,
-    link,
-}) => {
+const BlogCard = (blog) => {
     return (
         <article className="p-6 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <div className="flex justify-between items-center mb-5 text-gray-500">
                 <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                    <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
-                    </svg>
-                    {category}
+                   
+                    {blog.title_blog}
                 </span>
-                <span className="text-sm">{timeAgo}</span>
+                <span className="text-sm">{formatISODate(blog.created_at)}</span>
             </div>
 
-            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                <a href={link}>{title}</a>
-            </h2>
+            {/*<h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">*/}
+            {/*    <a href={link}>{title}</a>*/}
+            {/*</h2>*/}
 
-            <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{description}</p>
+            <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{blog.blog_message}</p>
 
             <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-4">
-                    <img className="w-7 h-7 rounded-full" src={avatarUrl} alt={`${author} avatar`} />
-                    <span className="font-medium dark:text-white">{author}</span>
+                    <img className="w-7 h-7 rounded-full" src={blog.user.image} alt={`${blog.user.nom} avatar`} />
+                    <span className="font-medium dark:text-white">{blog.user.nom}</span>
                 </div>
-                <a href={link} className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                <a href={"/login"} className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                     Read more
                     <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
@@ -114,23 +104,35 @@ const BlogCard = ({
     );
 };
 
-const blogPosts = [
-    {
-        category: "Tutorial",
-        timeAgo: "14 days ago",
-        title: "How to quickly deploy a static website",
-        description: "Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.",
-        author: "Jese Leos",
-        avatarUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-        link: "/home",
-    },
-    {
-        category: "Article",
-        timeAgo: "14 days ago",
-        title: "Our first project with React",
-        description: "Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.",
-        author: "Bonnie Green",
-        avatarUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
-        link: "/home",
-    },
-];
+function formatISODate(isoDateStr) {
+    const date = new Date(isoDateStr);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const year = date.getUTCFullYear();
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
+//const blogPosts = [
+//    {
+//        category: "Tutorial",
+//        timeAgo: "14 days ago",
+//        title: "How to quickly deploy a static website",
+//        description: "Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.",
+//        author: "Jese Leos",
+//        avatarUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
+//        link: "/home",
+//    },
+//    {
+//        category: "Article",
+//        timeAgo: "14 days ago",
+//        title: "Our first project with React",
+//        description: "Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.",
+//        author: "Bonnie Green",
+//        avatarUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+//        link: "/home",
+//    },
+//];
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -280,8 +280,8 @@ const NavbarHeader = () => {
                     />
                 </svg>
             ) : (
-                <svg class="w-[22px] h-[22px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd" />
+                <svg className="w-[22px] h-[22px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fillRule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clipRule="evenodd" />
                 </svg>
 
             ),
@@ -293,8 +293,8 @@ const NavbarHeader = () => {
             logo:
                 (currentNav === "about") ?
                     (
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd" />
+                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clipRule="evenodd" />
                         </svg>
 
                     )
@@ -345,8 +345,8 @@ const NavbarHeader = () => {
                     )
                     :
                     (
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clip-rule="evenodd" />
+                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fillRule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clipRule="evenodd" />
                         </svg>
                     )
             ,
