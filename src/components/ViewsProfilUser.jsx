699b35560a7069b4a76b971fca1c594817a5ead1
Marius_DIATTA <marius@kayfo.games>
2025-08-05 04:24:38 +0200

    add follow views

--- src/components/ViewsProfilUser.jsx
+++ src/components/ViewsProfilUser.jsx
@@ -33,10 +33,14 @@ const ViewsProfil = ({ clientId }) => {
                 !isLoading &&
                 <>
                 {
-                    (isFollow?.is_following)?
-                    <>
-                       {t(' followed')}
-                    </>
+                    (isFollow?.is_following) ?
+                    <div className="flex items-center text-sm">
+                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M5 11.917 9.724 16.5 19 7.5" />
+                        </svg>
+
+                        <>{t('followed')}</>
+                    </div>
                     :
                     <button
 
