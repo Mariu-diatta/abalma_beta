1d517253696e9a0968dc465568bbf16dcda7750d
Marius_DIATTA <marius@kayfo.games>
2025-08-05 18:12:29 +0200

    add protected router

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -3,12 +3,15 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = () => {
+
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
     const token = localStorage.getItem("token");
 
-    const hasAccess = isAuthenticated || Boolean(token);
+    const hasAccess = isAuthenticated &&  Boolean(token);
 
     if (!hasAccess) {
+
         return <Navigate to="/logIn" replace />;
     }
 
