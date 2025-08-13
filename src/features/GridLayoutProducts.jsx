d3cc4ed1b6158f038daa6d041774aa964473b4ec
Marius_DIATTA <marius@kayfo.games>
2025-08-13 20:01:07 +0200

    Add title on image product

--- src/features/GridLayoutProducts.jsx
+++ src/features/GridLayoutProducts.jsx
@@ -99,7 +99,7 @@ export function Carousel({ products }) {
                             key={idx}
                             src={prod.image_product}
                             alt={`Slide ${idx + 1}`}
-                            title="Le document de la donne"
+                            title={"Nombre d'articles disponibles: "+prod.quantity_product}
                             className={`absolute top-1/2 left-1/2 w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 transition-opacity duration-700 ease-in-out ${idx === currentIndex ? "opacity-100 z-20" : "opacity-0 z-10"
                                 }`}
                         />
