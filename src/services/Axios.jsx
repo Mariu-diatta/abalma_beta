73ee56ae9e668f220c8b230f217788f4a5c435a8
Marius_DIATTA <marius@kayfo.games>
2025-09-04 07:53:24 +0200

    lancer en prod version test

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 //local
-export const BASE_URL_ = 'http://127.0.0.1:8000/'; 
-export const BASE_URL = 'http://127.0.0.1:8000/';
+//export const BASE_URL_ = 'http://127.0.0.1:8000/'; 
+//export const BASE_URL = 'http://127.0.0.1:8000/';
 
 //Production
-//export const BASE_URL_ = 'https://backend-mpb0.onrender.com/'; 
-//export const BASE_URL = 'https://backend-mpb0.onrender.com/';
+export const BASE_URL_ = 'https://backend-mpb0.onrender.com/'; 
+export const BASE_URL = 'https://backend-mpb0.onrender.com/';
 
 
 const api = axios.create({
