7dfd3820266672fd122698bbbaf6a33033c94a70
Marius_DIATTA <marius@kayfo.games>
2025-09-10 10:36:20 +0200

    update titleCompgen

--- src/components/TitleComponentGen.jsx
+++ src/components/TitleComponentGen.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const TitleCompGen = ({title}) => {
 
     return (
-        <h1 className="animate-scroll overflow-x-auto whitespace-nowrap text-2xl font-extrabold text-gray-500 dark:text-white px-4 pt-4 pb-4 ">
-            {title}
-        </h1>
+        <div className="relative overflow-hidden w-full px-4 pt-4 pb-4">
+            <h1 className="animate-scroll inline-block whitespace-nowrap text-2xl font-extrabold text-gray-500 dark:text-white">
+                {title}
+            </h1>
+
+        </div>
+
     )
 }
 
