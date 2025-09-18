eb297702bb39d3018cf557eeb40e372d49309c94
Marius_DIATTA <marius@kayfo.games>
2025-09-18 18:52:19 +0200

    update AlertMessage

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -33,46 +33,41 @@ const AttentionAlertMessage = () => {
     if (!show) return ;
 
     return (
-        <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-80 w-full max-w-xl bg-transparent shadow-lg backdrop-blur-md">
+        <div className="fixed top-8 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg p-4 md:py-6 shadow-md bg-white dark:bg-dark-2 text-sm sm:text-base z-80 max-w-xl w-auto bg-transparent shadow-lg backdrop-blur-md flex">
 
-            <div className="relative flex w-full rounded-lg p-4 md:py-6 shadow-md bg-white dark:bg-dark-2 text-sm sm:text-base">
+            {/* Barre animée à gauche */}
+            <div className="absolute top-0 left-0 h-full w-[6px] overflow-hidden rounded-l-lg">
 
-                {/* Barre animée à gauche */}
-                <div className="absolute top-0 left-0 h-full w-[6px] overflow-hidden rounded-l-lg">
-
-                    <div
-                          className={`
-                          h-full w-full
-                          origin-top
-                          animate-drain 
-                          ${isError ? "bg-red-500" : "bg-blue-500"}
-                        `}
-                    >
-                    </div>
-
-                </div>
-
-                {/* Contenu alerte */}
-                <div className="ml-3 flex items-start gap-3">
-
-                    {/* Icône */}
-                    <div className="h-[34px] w-[34px] mt-1">{/* SVG ici */}</div>
-
-                    <div>
-
-                        <h5 className={`mb-1 text-lg font-semibold ${isError ? "text-red-600" : "text-blue-700"}`}>
-                            {messageAlert?.Type}
-                        </h5>
-
-                        <p className={`${isError ? "text-red-500" : "text-blue-600"}`}>
-                            {messageAlert?.Message}
-                        </p>
-
-                    </div>
+                <div
+                        className={`
+                        h-full w-full
+                        origin-top
+                        animate-drain 
+                        ${isError ? "bg-red-500" : "bg-blue-500"}
+                    `}
+                >
                 </div>
 
             </div>
 
+            {/* Contenu alerte */}
+            <div className="ml-3 flex items-start gap-3">
+
+                {/* Icône */}
+                <div className="h-[34px] w-[34px] mt-1">{/* SVG ici */}</div>
+
+                <div>
+
+                    <h5 className={`mb-1 text-lg font-semibold ${isError ? "text-red-600" : "text-blue-700"}`}>
+                        {messageAlert?.Type}
+                    </h5>
+
+                    <p className={`${isError ? "text-red-500" : "text-blue-600"}`}>
+                        {messageAlert?.Message}
+                    </p>
+
+                </div>
+            </div>
         </div>
     );
 };
