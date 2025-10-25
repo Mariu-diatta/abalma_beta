d4b41ba6da8b38b021f243e8c2cc5fd8b1db7a8d
Marius_DIATTA <marius@kayfo.games>
2025-10-25 13:11:47 +0200

    desabled log message on GridProductDefaultSize

--- src/features/GridProductDefaultSize.jsx
+++ src/features/GridProductDefaultSize.jsx
@@ -21,7 +21,6 @@ import {
 import OwnerAvatar from "../components/OwnerProfil";
 import ProductModal from "../pages/ProductViewsDetails";
 import LoadingCard from "../components/LoardingSpin";
-import { productViews } from "../utils";
 import { useTranslation } from 'react-i18next';
 import PrintNumberStars from "../components/SystemStar";
 import ScrollingContent from "../components/ScrollContain";
@@ -41,7 +40,6 @@ const GridProductDefault = ({ categorie_item }) => {
     const [owners, setOwners] = useState({});
     const [modalData, setModalData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
-    const [productNbViews, setProductNbViews] = useState(null);
     const categorySelectedData = useSelector(state => state?.navigate?.categorySelectedOnSearch)
 
 
@@ -155,119 +153,120 @@ const GridProductDefault = ({ categorie_item }) => {
                         <div className="grid grid-cols-2 md:grid-cols-5 gap-1 mt-2 w-[100dvw] md:w-auto mx-auto overflow-x-hidden">
 
                         {
-                            cols?.map((products, colIdx) => (
+                            cols?.map(
 
-                                <div key={colIdx} className="grid gap-1">
+                                (products, colIdx) => (
 
-                                    {
-                                        products.map(product => {
+                                    <div key={colIdx} className="grid gap-1">
 
-                                                const isInCart = cartItems.some(p => p.id === product.id);
+                                        {
+                                            products.map(
 
-                                                const owner = owners[product.fournisseur];
+                                                product => {
 
-                                            //const p=productViews(product, setProductNbViews);
+                                                    const isInCart = cartItems.some(p => p.id === product.id);
 
-                                            console.log(product)
+                                                    const owner = owners[product.fournisseur];
 
-                                                return (
+                                                    return (
 
-                                                    <div
-                                                        key={product?.id}
-                                                        className={`w-[50dvw] md:w-50 min-h-50 rounded-lg  transition transform hover:-translate-y-1 ${isInCart
-                                                                ? "opacity-50 pointer-events-none bg-gray-100"
-                                                                : "bg-white"
-                                                            }`}
-                                                        style={{
-                                                            backgroundColor: "var(--color-bg)",
-                                                            color: "var(--color-text)"
-                                                        }}
-                                                    >
                                                         <div
-                                                            className="relative w-full block rounded-lg overflow-hidden"
-                                                            aria-label={`Voir le produit ${product?.product_name}`}
+                                                            key={product?.id}
+                                                            className={`w-[50dvw] md:w-50 min-h-50 rounded-lg  transition transform hover:-translate-y-1 ${isInCart
+                                                                    ? "opacity-50 pointer-events-none bg-gray-100"
+                                                                    : "bg-white"
+                                                                }`}
+                                                            style={{
+                                                                backgroundColor: "var(--color-bg)",
+                                                                color: "var(--color-text)"
+                                                            }}
                                                         >
-                                                            <img
-                                                                src={product.image_product}
-                                                                alt={"alt_prod"}
-                                                                onClick={() => {
-                                                                    openModal(product);
-                                                                    dispatch(addUser(owner));
-                                                                }}
-                                                                loading="lazy"
-                                                                className="h-auto w-full rounded-lg transition duration-300 ease-in-out hover:brightness-75 hover:grayscale"
-                                                            />
-                                                        </div>
-
-                                                        <div className="p-1">
-
-                                                            <div className="flex justify-between items-center mb-1">
-
-                                                                <OwnerAvatar owner={owner} />
-
-                                                                {product?.quantity_product !== "0" && (
-                                                                    <span className="text-xs text-gray-600">
-                                                                        {t("quantity")} {product?.quantity_product}
-                                                                    </span>
-                                                                )}
-                                                            </div>
-
-                                                            <PrintNumberStars productNbViews={product?.total_views} t={t} />
-
-                                                            <p className="text-xs truncate mb-1 md:text-sm">
-                                                                {product?.description_product}
-                                                            </p>
-
-                                                            <div className="whitespace-nowrap flex text-xs gap-1 md:hidden dark:bg-white-100 p-1 rounded-lg">
-                                                                <p>{t('quantity_sold')}</p>{product?.quatity_sold}
-                                                            </div>
-
-                                                            <div className="flex justify-between items-center">
-
-                                                                <ScrollingContent
-                                                                    item={product}
-                                                                    t={t}
-                                                                    qut_sold={product?.quatity_sold}
-                                                                />
-
-                                                                <button
-                                                                    title="Ajouter au panier"
+                                                            <div
+                                                                className="relative w-full block rounded-lg overflow-hidden"
+                                                                aria-label={`Voir le produit ${product?.product_name}`}
+                                                            >
+                                                                <img
+                                                                    src={product.image_product}
+                                                                    alt={"alt_prod"}
                                                                     onClick={() => {
-                                                                        dispatch(addToCart(product));
-                                                                        dispatch(
-                                                                            addMessageNotif(
-                                                                                `Produit ${product?.code_reference} sélectionné le ${Date.now()}`
-                                                                            )
-                                                                        );
+                                                                        openModal(product);
+                                                                        dispatch(addUser(owner));
                                                                     }}
-                                                                    className="cursor-pointer p-1 rounded-full hover:bg-green-100 transition"
-                                                                >
-                                                                    <svg
-                                                                        className="w-8 h-6 text-gray-800 dark:text-white border-1 rounded-lg"
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        fill="none"
-                                                                        viewBox="0 0 24 24"
-                                                                    >
-                                                                        <path
-                                                                            stroke="currentColor"
-                                                                            strokeLinecap="round"
-                                                                            strokeLinejoin="round"
-                                                                            strokeWidth="1"
-                                                                            d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4"
-                                                                        />
-                                                                    </svg>
+                                                                    loading="lazy"
+                                                                    className="h-auto w-full rounded-lg transition duration-300 ease-in-out hover:brightness-75 hover:grayscale"
+                                                                />
+                                                            </div>
 
-                                                                </button>
+                                                            <div className="p-1">
+
+                                                                <div className="flex justify-between items-center mb-1">
+
+                                                                    <OwnerAvatar owner={owner} />
+
+                                                                    {product?.quantity_product !== "0" && (
+                                                                        <span className="text-xs text-gray-600">
+                                                                            {t("quantity")} {product?.quantity_product}
+                                                                        </span>
+                                                                    )}
+                                                                </div>
+
+                                                                <PrintNumberStars productNbViews={product?.total_views} t={t} />
+
+                                                                <p className="text-xs truncate mb-1 md:text-sm">
+                                                                    {product?.description_product}
+                                                                </p>
+
+                                                                <div className="whitespace-nowrap flex text-xs gap-1 md:hidden dark:bg-white-100 p-1 rounded-lg">
+                                                                    <p>{t('quantity_sold')}</p>{product?.quatity_sold}
+                                                                </div>
+
+                                                                <div className="flex justify-between items-center">
+
+                                                                    <ScrollingContent
+                                                                        item={product}
+                                                                        t={t}
+                                                                        qut_sold={product?.quatity_sold}
+                                                                    />
+
+                                                                    <button
+                                                                        title="Ajouter au panier"
+                                                                        onClick={() => {
+                                                                            dispatch(addToCart(product));
+                                                                            dispatch(
+                                                                                addMessageNotif(
+                                                                                    `Produit ${product?.code_reference} sélectionné le ${Date.now()}`
+                                                                                )
+                                                                            );
+                                                                        }}
+                                                                        className="cursor-pointer p-1 rounded-full hover:bg-green-100 transition"
+                                                                    >
+                                                                        <svg
+                                                                            className="w-8 h-6 text-gray-800 dark:text-white border-1 rounded-lg"
+                                                                            xmlns="http://www.w3.org/2000/svg"
+                                                                            fill="none"
+                                                                            viewBox="0 0 24 24"
+                                                                        >
+                                                                            <path
+                                                                                stroke="currentColor"
+                                                                                strokeLinecap="round"
+                                                                                strokeLinejoin="round"
+                                                                                strokeWidth="1"
+                                                                                d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4"
+                                                                            />
+                                                                        </svg>
+
+                                                                    </button>
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                );
-                                            }
-                                        )
-                                    }
+                                                    );
+                                                }
+                                            )
+                                        }
 
-                                </div>
-                            ))
+                                    </div>
+                                )
+                            )
                         }
                         </div>
                     ) 
