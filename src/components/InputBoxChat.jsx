8b8daedf3a3053109149ef712d6b44077f1c07aa
Marius_DIATTA <marius@kayfo.games>
2026-01-07 01:58:16 +0100

    update button

--- src/components/InputBoxChat.jsx
+++ src/components/InputBoxChat.jsx
@@ -27,11 +27,13 @@ const InputBoxChat = ({ allRoomsChats, input, setInput, setShow, sendMessage, ha
                     aria-label="Envoyer"
                 >
                     <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+
                         <path
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M12 2a1 1 0 0 1 .932.638l7 18a1 1 0 0 1-1.326 1.281L13 19.517V13a1 1 0 1 0-2 0v6.517l-5.606 2.402a1 1 0 0 1-1.326-1.281l7-18A1 1 0 0 1 12 2Z"
                         />
+
                     </svg>
 
                 </button>
