c781c6b63bd9191b2bce3a3ec01f7be34c0ed5fa
Marius_DIATTA <marius@kayfo.games>
2026-01-10 03:43:27 +0100

    add size input button on login

--- src/components/InputBoxFloat.jsx
+++ src/components/InputBoxFloat.jsx
@@ -12,9 +12,11 @@ const InputBox = ({
     required = false,
     ...rest
 }) => {
+
     const inputId = id || `input-${name}`;
 
     return (
+
         <div className="relative mb-6 w-auto">
 
             <input
@@ -27,15 +29,24 @@ const InputBox = ({
                 maxLength={maxLength}
                 required={required}
                 ref={ref}
-                className="block rounded-t-lg px-1  lg:px-2.5 pb-2.5 pt-5 w-full text-md text-gray-700  border-0 border-b-1 border-gray-300 appearance dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-100 peer"
+                className="
+                  block w-full px-2.5 pt-5 pb-2.5
+                  text-black dark:text-white
+                  bg-transparent
+                  border-b border-gray-300
+                  focus:outline-none focus:ring-0
+                  peer
+                "
                 {...rest}
             />
+
             <label
                 htmlFor={inputId}
                 className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
             >
                 {placeholder}
             </label>
+
         </div>
     );
 };
