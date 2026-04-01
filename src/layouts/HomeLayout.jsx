d9e6a54fbc57970547df3e8f6275b7f1f835bd96
Marius_DIATTA <marius@kayfo.games>
2026-04-02 01:51:08 +0200

    upload frontend

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect} from 'react';
+import React from 'react';
 import Footer from '../pages/Footer';
 import NavbarHeader from '../pages/Header';
-import { useDispatch, useSelector } from 'react-redux';
-//import { Navigate } from 'react-router-dom';
-import { setCurrentNav } from '../slices/navigateSlice';
+//import { useDispatch, useSelector } from 'react-redux'; , { useEffect} 
+////import { Navigate } from 'react-router-dom';
+//import { setCurrentNav } from '../slices/navigateSlice';
 import ScrollTop from '../components/ButtonScroll';
-import { ENDPOINTS } from '../utils';
+//import { ENDPOINTS } from '../utils';
 
 const HomeLayout = ({ children }) => {
 
-    const dispatch = useDispatch();
+    //const dispatch = useDispatch();
 
-    const currentUser = useSelector((state) => state.auth.user);
+    //const currentUser = useSelector((state) => state.auth.user);
 
-    useEffect(() => {
+    //useEffect(() => {
 
-        if (currentUser) {
+    //    if (currentUser) {
 
-            dispatch(setCurrentNav(ENDPOINTS.ACCOUNT_HOME));
-        }
+    //        dispatch(setCurrentNav(ENDPOINTS.ACCOUNT_HOME));
+    //    }
 
-    }, [currentUser, dispatch]);
+    //}, [currentUser, dispatch]);
 
     //useEffect(
 
