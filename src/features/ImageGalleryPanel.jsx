1f56009ce88d1d694e9c713c3a0ab55cee06f4c7
Marius_DIATTA <marius@kayfo.games>
2026-04-13 05:11:41 +0200

    make space on images products

--- src/features/ImageGalleryPanel.jsx
+++ src/features/ImageGalleryPanel.jsx
@@ -11,7 +11,7 @@ function CategoryImagesDisplay({ products, openModal, owners }) {
 
     return (
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-0.5 scrollbor_hidden_ overflow-y-auto h-full w-full pb-0 pt-0 p-0 m-0">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-0.5 scrollbor_hidden_ overflow-y-auto h-full w-full p-2 mx-1">
 
             {
                 imagesProductsVariants?.map((item,k)=> {
