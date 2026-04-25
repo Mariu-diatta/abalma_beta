c3a9424a0b441658360efa45e91802e4b40a7666
Marius_DIATTA <marius@kayfo.games>
2026-04-25 05:25:16 +0200

    add createPortal and set auto to overflow-y

--- src/features/ButtonPaymentShopping.jsx
+++ src/features/ButtonPaymentShopping.jsx
@@ -36,9 +36,9 @@ const BuyButtonWithPaymentForm = ({ total_price, reference }) => {
             {
                 showForm && (
 
-                    <div className="z-[9999] backdrop-blur-sm fixed inset-0 z-50 bg-gray-100 bg-transparent  bg-opacity-100 flex items-center justify-center style-bg" onClick={() => setShowPaymentForm(false)}>
+                    <div className="m-5 z-[9999] backdrop-blur-sm fixed inset-0 z-50 bg-gray-100 bg-transparent  bg-opacity-100 flex items-center justify-center style-bg" onClick={() => setShowPaymentForm(false)}>
 
-                        <div className=" w-full h-full overflow-y-auto rounded-lg p-6 shadow-xl " onClick={(e) => e.stopPropagation()}>
+                        <div className=" w-full h-full rounded-lg p-6 shadow-xl " onClick={(e) => e.stopPropagation()}>
 
                             <div className="flex justify-between items-center mb-4 style-bg">
 
