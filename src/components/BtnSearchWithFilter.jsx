26b0c68e0aa8df6e644858966ff4cbdf141e383a
Marius_DIATTA <marius@kayfo.games>
2026-05-14 15:42:39 +0200

    set icon on button search and button categories

--- src/components/BtnSearchWithFilter.jsx
+++ src/components/BtnSearchWithFilter.jsx
@@ -88,8 +88,8 @@ const SearchBar = ({disabled = false }) => {
                     className="px-2.5 border-r-0 border border-gray-100   text-sm z-10 text-sm text-gray-900  rounded-l-full dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring-0 "
                     disabled={disabled}
                 >
-                    <svg className="w-4 h-4" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M19 19L15 15M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeWidth="1" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
                     </svg>
 
                     <span className="sr-only">{t("Search")}</span>
