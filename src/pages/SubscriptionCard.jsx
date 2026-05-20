18bc9cd87f3912d3c48ec917e8a68386bda581e8
Marius_DIATTA <marius@kayfo.games>
2026-05-20 22:12:30 +0200

    add disable button connection when user not subscript

--- src/pages/SubscriptionCard.jsx
+++ src/pages/SubscriptionCard.jsx
@@ -122,7 +122,7 @@ function SubscriptionCard({
 
             <button
                 onClick={() => handleSubscribe(currentUser?.email, amount_id)}
-                disabled={!currentUser}
+                disabled={!currentUser && !currentUser?.is_subscribed}
                 className={`
                 ${IS_USE_SUBCRIB ? "bg-gray-200  cursor-not-allowed" :" bg-blue-200"} py-2.5 font-semibold my-1 rounded-md
                 hover:bg-gray-300 transition
@@ -239,7 +239,7 @@ export default function SubscriptionsPage() {
 
     return (
 
-        <main className=" flex flex-col items-center justify-center  bg-none mx-2 mt-16 overflow-y-auto h-full pt-[20dvh] scrollbor_hidden">
+        <main className=" flex flex-col items-center justify-center  bg-none mx-2 mt-16 overflow-y-auto h-full pt-[20dvh] scrollbor_hidden ">
 
             <button
 
