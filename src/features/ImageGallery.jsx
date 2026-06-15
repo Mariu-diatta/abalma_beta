7daae8bcdf124d18a95afa0fe6b5da065c586630
Marius_DIATTA <marius@kayfo.games>
2026-06-15 02:53:58 +0200

    add scroll

--- src/features/ImageGallery.jsx
+++ src/features/ImageGallery.jsx
@@ -8,24 +8,27 @@ const ImageGallery = ({ imagesEls,  openModal, owners}) => {
 
     return (
 
-        <div className="grid grid-cols-2 gap-1 h-full w-full overflow-y-auto overflow-x-auto scrollbor_hidden z-0 p-5 m-0">
+        <div className="grid grid-cols-2 gap-1 scrollbor_hidden overflow-y-auto h-full w-full mx-1 hidden lg:block">
 
 
-            {imagesEls?.map(item => {
-                const isInCart = cartItems?.some(product => product?.id === item?.id);
-                const owner = owners[item?.fournisseur];
+            {
+                imagesEls?.map(item => {
 
-                return (
-                    <ProductCard
-                        key={item?.id} // ✅ Clé unique ici
-                        item={item}
-                        isInCart={isInCart}
-                        owner={owner}
-                        openModal={openModal}
-                        owners={owners}
-                    />
-                );
-            })}
+                    const isInCart = cartItems?.some(product => product?.id === item?.id);
+                    const owner = owners[item?.fournisseur];
+
+                    return (
+                        <ProductCard
+                            key={item?.id} // ✅ Clé unique ici
+                            item={item}
+                            isInCart={isInCart}
+                            owner={owner}
+                            openModal={openModal}
+                            owners={owners}
+                        />
+                    );
+                })
+            }
 
         </div>
     )
--- src/features/ProductSpecificPopovViews.jsx
+++ src/features/ProductSpecificPopovViews.jsx
@@ -16,7 +16,7 @@ const ProductSpecifiViews = ({ products, openModal, owners, panelRef }) => {
             className={`flex gap-0 gap-1 bg-grey-9000 shadow-xs rounded-md  mt-1 z-0 py-1 m-0 ${products?.length <= 0 ? "hidden" : "min-h-70 max-h-70 w-full"}`}
 
         >
-            <div style={{ flex: 2 }} className="hidden lg:block">
+            <div style={{ flex: 2 }}>
 
                 <CategoryProductsCardDisplay
 
